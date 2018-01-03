65061186225e6cd856a0af9c11e6383297a55210
jinyalin <jinyalin@baidu.com>
2018-01-03 20:54:32 +0800

    add another test data

--- course01/plandar_data_classification/my_nn_model.py
+++ course01/plandar_data_classification/my_nn_model.py
@@ -1,16 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from testCases_v2 import *
-import sklearn
-import sklearn.datasets
-import sklearn.linear_model
 from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
 
 X, Y = load_planar_dataset()
-
-shape_X = np.shape(X)
-shape_Y = np.shape(Y)
-m = X.shape[1]
 
 
 def layer_sizes(X, Y):
@@ -43,7 +36,7 @@ def forward_propagation(X, parameters):
     A1 = np.tanh(Z1)
     # A1 = sigmoid(Z1)
     # A1 = Z1 * (Z1 >= 0)
-    Z2 = np.dot(W2, Z1) + b2
+    Z2 = np.dot(W2, A1) + b2
     A2 = sigmoid(Z2)
 
     cache = {"Z1": Z1,
@@ -112,3 +105,74 @@ def update_parameters(parameters, grads, learning_rate=1.2):
                   "b2": b2}
 
     return parameters
+
+
+def nn_model(X, Y, n_h, num_iterations=10000, print_cost=False):
+    n_x = layer_sizes(X, Y)[0]
+    n_y = layer_sizes(X, Y)[2]
+
+    parameters = initialize_parameters(n_x, n_h, n_y)
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+
+    for i in range(0, num_iterations):
+        A2, cache = forward_propagation(X, parameters)
+
+        cost = compute_cost(A2, Y, parameters)
+
+        grads = backward_propagation(parameters, cache, X, Y)
+
+        parameters = update_parameters(parameters, grads, learning_rate=0.05)
+
+        if print_cost and i % 1000 == 0:
+            print("Cost after iteration %i: %f" % (i, cost))
+
+    return parameters
+
+
+def predict(parameters, X):
+    A2, cache = forward_propagation(X, parameters)
+    predictions = (A2 > 0.5) * 1
+    return predictions
+
+
+parameters = nn_model(X, Y, n_h=4, num_iterations=20000, print_cost=True)
+
+plot_decision_boundary(lambda x: predict(parameters, x.T), X, Y)
+plt.title("Decision Boundary for hidden layer size " + str(4))
+
+predictions = predict(parameters, X)
+print('Accuracy: %d' % float((np.dot(Y, predictions.T) + np.dot(1 - Y, 1 - predictions.T)) / float(Y.size) * 100) + '%')
+
+# plt.figure(figsize=(16, 32))
+# hidden_layer_sizes = [1, 2, 3, 4, 5, 20, 50]
+# for i, n_h in enumerate(hidden_layer_sizes):
+#     plt.subplot(5, 2, i + 1)
+#     plt.title('Hidden Layer of size %d' % n_h)
+#     parameters = nn_model(X, Y, n_h, num_iterations=20000)
+#     plot_decision_boundary(lambda x: predict(parameters, x.T), X, Y)
+#     predictions = predict(parameters, X)
+#     accuracy = float((np.dot(Y, predictions.T) + np.dot(1 - Y, 1 - predictions.T)) / float(Y.size) * 100)
+#     print("Accuracy for {} hidden units: {} %".format(n_h, accuracy))
+
+noisy_circles, noisy_moons, blobs, gaussian_quantiles, no_structure = load_extra_datasets()
+
+datasets = {"noisy_circles": noisy_circles,
+            "noisy_moons": noisy_moons,
+            "blobs": blobs,
+            "gaussian_quantiles": gaussian_quantiles}
+
+dataset = "noisy_moons"
+
+X, Y = datasets[dataset]
+X, Y = X.T, Y.reshape(1, Y.shape[0])
+
+# make blobs binary
+if dataset == "blobs":
+    Y = Y % 2
+
+# Visualize the data
+plt.scatter(X[0, :], X[1, :], c=Y, s=40, cmap=plt.cm.Spectral)
+plt.show()
--- course01/plandar_data_classification/planar_utils.py
+++ course01/plandar_data_classification/planar_utils.py
@@ -52,7 +52,7 @@ def load_extra_datasets():
     N = 200
     noisy_circles = sklearn.datasets.make_circles(n_samples=N, factor=.5, noise=.3)
     noisy_moons = sklearn.datasets.make_moons(n_samples=N, noise=.2)
-    blobs = sklearn.datasets.make_blobss(n_samples=N, random_state=5, n_features=2, centers=6)
+    blobs = sklearn.datasets.make_blobs(n_samples=N, random_state=5, n_features=2, centers=6)
     gaussian_quantiles = sklearn.datasets.make_gaussian_quantiles(mean=None, cov=.5, n_samples=N, n_features=2,
                                                                   n_classes=2, shuffle=True, random_state=None)
     no_structure = np.random.rand(N, 2), np.random.rand(N, 2)
