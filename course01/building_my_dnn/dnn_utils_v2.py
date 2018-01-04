86fe7749ad116674bf4d8194f671cb71cfb85028
jinyalin <jinyalin@baidu.com>
2018-01-04 10:51:01 +0800

    add building my dnn notebook

---
+++ course01/building_my_dnn/dnn_utils_v2.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def sigmoid(Z):
+    A = 1 / (1 + np.exp(-Z))
+    cache = Z
+    return A, cache
+
+
+def sigmoid_backward(dA, cache):
+    Z = cache
+    s = 1 / (1 + np.exp(-Z))
+    dZ = dA * s * (1 - s)
+    assert (dZ.shape == Z.shape)
+    return dZ
+
+
+def relu(Z):
+    A = np.maximum(0, Z)
+    assert (A.shape == Z.shape)
+    cache = Z
+    return A, cache
+
+
+def relu_backward(dA, cache):
+    Z = cache
+    dZ = np.array(dA, copy=True)
+
+    dZ[Z <= 0] = 0
+    assert (dZ.shape == Z.shape)
+
+    return dZ
