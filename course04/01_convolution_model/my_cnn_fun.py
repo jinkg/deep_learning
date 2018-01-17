d6a02c6c0a866c979b3dd2ffdf8c5414e172b97b
jinyalin <jinyalin@baidu.com>
2018-01-17 09:25:45 +0800

    conv backward

--- course04/01_convolution_model/my_cnn_fun.py
+++ course04/01_convolution_model/my_cnn_fun.py
@@ -184,3 +184,69 @@ if __name__ == "__main__":
     A, cache = pool_forward(A_prev, hparameters, mode="average")
     print("mode = average")
     print("A =", A)
+
+
+def conv_backward(dZ, cache):
+    """
+       Implement the backward propagation for a convolution function
+
+       Arguments:
+       dZ -- gradient of the cost with respect to the output of the conv layer (Z), numpy array of shape (m, n_H, n_W, n_C)
+       cache -- cache of values needed for the conv_backward(), output of conv_forward()
+
+       Returns:
+       dA_prev -- gradient of the cost with respect to the input of the conv layer (A_prev),
+                  numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+       dW -- gradient of the cost with respect to the weights of the conv layer (W)
+             numpy array of shape (f, f, n_C_prev, n_C)
+       db -- gradient of the cost with respect to the biases of the conv layer (b)
+             numpy array of shape (1, 1, 1, n_C)
+    """
+    (A_prev, W, b, hparameters) = cache
+
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+
+    (f, f, n_C_prev, n_C) = W.shape
+
+    stride = hparameters['stride']
+    pad = hparameters['pad']
+
+    (m, n_H, n_W, n_C) = dZ.shape
+
+    dA_prev = np.zeros((A_prev.shape))
+    dW = np.zeros((W.shape))
+    db = np.zeros((b.shape))
+
+    A_prev_pad = zero_pad(A_prev, pad)
+    dA_prev_pad = zero_pad(dA_prev, pad)
+
+    for i in range(m):
+        a_prev_pad = A_prev_pad[i]
+        da_prev_pad = dA_prev_pad[i]
+        for h in range(n_H):
+            for w in range(n_W):
+                for c in range(n_C):
+                    vert_start = h * stride
+                    vert_end = vert_start + f
+                    horiz_start = w * stride
+                    horiz_end = horiz_start + f
+
+                    a_slice = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
+
+                    da_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :] += W[:, :, :, c] * dZ[i, h, w, c]
+                    dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
+                    db[:, :, :, c] += dZ[i, h, w, c]
+
+        dA_prev[i, :, :, :] = da_prev_pad[pad:-pad, pad:-pad, :]
+
+    assert (dA_prev.shape == (m, n_H_prev, n_W_prev, n_C_prev))
+
+    return dA_prev, dW, db
+
+
+if __name__ == "__main__":
+    np.random.seed(1)
+    dA, dW, db = conv_backward(Z, cache_conv)
+    print("dA_mean =", np.mean(dA))
+    print("dW_mean =", np.mean(dW))
+    print("db_mean =", np.mean(db))
